29cba83c83dac9cf7f67f23e76004e85bc0b8f8d
Ian Homer <ian.homer@gmail.com>
2019-04-10 21:21:50 +0100

    Add more graphql examples

--- README.md
+++ README.md
@@ -9,4 +9,8 @@ and access
 
     http://localhost:4000/graphql    
   
+# thanks to
+
+* https://graphql.github.io/learn/
+* https://www.howtographql.com/
 
--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,57 @@ import pino from "pino";
 const logger = pino();
 
 const schema = buildSchema(`
+  type User {
+    id: String
+    name: String
+    age: Int
+  }
+    
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    user(id: String): User
+    users: [User]
+    rollDice(numDice: Int!, numSides: Int): [Int]
   }
 `);
 
-const root = { hello: () => "Hello world!" };
+const rollDice = ({ numDice, numSides }) => {
+  const output = [];
+  for (const i = 0; i < numDice; i++) {
+    output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+  }
+  return output;
+};
+
+const USERS : { [key: string]: any } = {
+  "abc": {
+    age: 25,
+    id: "abc",
+    name: "Andy"
+  },
+    "def": {
+        age: 28,
+        id: "def",
+        name: "Betty"
+    }
+};
+
+const getUser = ({ id }) => {
+  return USERS[id];
+};
+
+const getUsers = () => {
+    return Object.values(USERS);
+};
+
+const root = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+  },
+  rollDice,
+  user: getUser,
+    users: getUsers
+};
 
 const app = express();
 app.use(

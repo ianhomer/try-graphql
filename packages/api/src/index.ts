d58f146430f1c1efb1c56b875cd41394e8674da7
Ian Homer <code@ianhomer.com>
2023-01-04 21:21:22 +0100

    resolver test (#23)

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,9 +4,9 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 
 console.log("Initialising API ...");
-dotenv.config({ path: __dirname + "/../.env", debug: true });
+dotenv.config({ path: __dirname + "/../.env" });
 
-import resolvers from "./resolvers";
+import createResolvers from "./resolvers";
 import schema from "./schema";
 
 const app = express();
@@ -15,33 +15,33 @@ app.get("/", (_, res) => {
   res.send("GraphQL API is available at /graphql");
 });
 
-const root = resolvers;
+createResolvers().then((root) => {
+  app.use(cors());
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: true,
+    })
+  );
 
-app.use(cors());
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
+  const PORT = 8080;
+  const server = app.listen(PORT, () =>
+    console.log(`Running GraphQL server on http://localhost:${PORT}`)
+  );
 
-const PORT = 8080;
-const server = app.listen(PORT, () =>
-  console.log(`Running GraphQL server on http://localhost:${PORT}`)
-);
+  const closeHandler = async () => {
+    server.close(async (error: any) => {
+      console.log("GraphQL server closed");
+      if (error) {
+        console.error(`Error on close : ${error}`);
+      }
+      await root.close();
+      process.exit(error ? 1 : 0);
+    });
+  };
 
-const closeHandler = async () => {
-  server.close(async (error: any) => {
-    console.log("GraphQL server closed");
-    if (error) {
-      console.error(`Error on close : ${error}`);
-    }
-    await root.close();
-    process.exit(error ? 1 : 0);
-  });
-};
-
-process.on("SIGTERM", closeHandler);
-process.on("SIGINT", closeHandler);
+  process.on("SIGTERM", closeHandler);
+  process.on("SIGINT", closeHandler);
+});
--- packages/api/src/resolvers.ts
+++ packages/api/src/resolvers.ts
@@ -3,30 +3,32 @@ import axios from "axios";
 
 import Repository from "./repository";
 
-const repository = new Repository();
-repository.init();
+const createResolvers = async () => {
+  const repository = new Repository();
+  await repository.init();
 
-const resolvers = {
-  songs: () => {
-    return repository.findAllSongs();
-  },
-  summary: async () => {
-    const songs = await repository.findAllSongs();
-    const response = await axios<Song[]>({
-      method: "post",
-      url: "http://localhost:8081/api/echo",
-      data: songs,
-    });
-    console.log(
-      `${new Date().getTime()} : Summary count ${response.data.length}`
-    );
-    return {
-      count: response.data.length,
-    };
-  },
-  close: async () => {
-    return repository.close();
-  },
+  return {
+    songs: () => {
+      return repository.findAllSongs();
+    },
+    summary: async () => {
+      const songs = await repository.findAllSongs();
+      const response = await axios<Song[]>({
+        method: "post",
+        url: "http://localhost:8081/api/echo",
+        data: songs,
+      });
+      console.log(
+        `${new Date().getTime()} : Summary count ${response.data.length}`
+      );
+      return {
+        count: response.data.length,
+      };
+    },
+    close: async () => {
+      return repository.close();
+    },
+  };
 };
 
-export default resolvers;
+export default createResolvers;

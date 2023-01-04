7b5e299749b3df4c2c1ebd972fc72fc81659780f
Ian Homer <code@ianhomer.com>
2023-01-04 09:38:42 +0000

    Add e2e test into CI

--- README.md
+++ README.md
@@ -7,6 +7,19 @@
 
 and access <http://localhost:8080/graphql>
 
+## End to end test
+
+    pnpm start
+    pnpm test:e2e
+
+To see debug logs
+
+    export DEBUG=pw:webserver
+
+To enable CI mode
+
+    export CI=true
+
 ## thanks to
 
 - <https://graphql.github.io/learn/>
--- packages/end-to-end/playwright.config.ts
+++ packages/end-to-end/playwright.config.ts
@@ -1,11 +1,21 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
+
+const timeout = (process.env.CI ? 30 : 10) * 1000;
+
 const config: PlaywrightTestConfig = {
+  timeout,
   use: {
     baseURL: "http://localhost:3000",
     headless: false,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
     video: "on-first-retry",
+  },
+  webServer: {
+    command: "cd ../.. && pnpm start",
+    url: "http://127.0.0.1:3000",
+    timeout,
+    reuseExistingServer: !process.env.CI,
   },
 };
 export default config;

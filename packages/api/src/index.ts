eb0f2d55b2bd98b101960e47ca7da2728be238d5
Ian Homer <code@ianhomer.com>
2023-01-04 20:15:03 +0000

    spin up server on create resolvers promise

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -15,33 +15,33 @@ app.get("/", (_, res) => {
   res.send("GraphQL API is available at /graphql");
 });
 
-const root = createResolvers();
+createResolvers().then((root) => {
+  app.use(cors());
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: true,
+    })
+  );
 
-app.use(cors());
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
+  const PORT = 8080;
+  const server = app.listen(PORT, () =>
+    console.log(`Running GraphQL server on http://localhost:${PORT}`)
+  );
 
-const PORT = 8080;
-const server = app.listen(PORT, () =>
-  console.log(`Running GraphQL server on http://localhost:${PORT}`)
-);
+  const closeHandler = async () => {
+    server.close(async (error: any) => {
+      console.log("GraphQL server closed");
+      if (error) {
+        console.error(`Error on close : ${error}`);
+      }
+      await root.close();
+      process.exit(error ? 1 : 0);
+    });
+  };
 
-const closeHandler = async () => {
-  server.close(async (error: any) => {
-    console.log("GraphQL server closed");
-    if (error) {
-      console.error(`Error on close : ${error}`);
-    }
-    await root.close();
-    process.exit(error ? 1 : 0);
-  });
-};
-
-process.on("SIGTERM", closeHandler);
-process.on("SIGINT", closeHandler);
+  process.on("SIGTERM", closeHandler);
+  process.on("SIGINT", closeHandler);
+});

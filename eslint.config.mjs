115c8ee0f4359d0abe486649ad67f149689ca6fc
Ian Homer <code@ianhomer.com>
2024-09-23 22:11:01 +0100

    Upgrade eslint and husky configuration (#27)
    * 📦️ Upgrade eslint packages and config
    
    * 🔧 migrate to husky init not install
    
    * ⬆️ upgrade CI to node v22
    
    * 🔧 disable the ESlint plugin
    
    To avoid issue https://github.com/facebook/create-react-app/issues/13618
    
    * 🔧 disable eslint plugin in react scripts build

---
+++ eslint.config.mjs
@@ -0,0 +1,65 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import noOnlyTests from "eslint-plugin-no-only-tests";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import sonarjs from "eslint-plugin-sonarjs";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/dist/", "**/build/"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      "no-only-tests": noOnlyTests,
+      "simple-import-sort": simpleImportSort,
+      sonarjs,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+    },
+
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-var-requires": "off",
+
+      "no-only-tests/no-only-tests": [
+        "error",
+        {
+          fix: true,
+        },
+      ],
+
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "sonarjs/cognitive-complexity": "off",
+      eqeqeq: ["error", "smart"],
+    },
+  },
+];
